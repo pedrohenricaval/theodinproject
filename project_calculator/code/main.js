893cc4e6d7b95f2b1fa59b1352d1e574755f9544
Pedro Lima <pedrohenricaval@gmail.com>
2024-07-03 17:27:34 -0300

    Criada funcao math

--- project_calculator/code/main.js
+++ project_calculator/code/main.js
@@ -1,9 +1,45 @@
-function main (){
-    
+function math (operation, ...array){
+    switch(operation){
+        case "+":
+            return add(array);
+            break;
+
+        case "-":
+            return sub(array);
+            break;
+
+        case "*":
+            return multiply(array);
+            break;
+
+        case "/":
+            return divide(array);
+            break;
+    }
+}
+
+/*funcoes math*/
+function add (array){
+    return array.reduce( (total, element) => total+element )
+}
+
+function sub (array){
+    return array.reduce( (total, element) => total-element )
+}
+
+function multiply (array){
+    return array.reduce( (total, element) => total*element )
+    return array.reduce((product, current) => product * current)
+}
+
+function divide (array){
+    return array.reduce( (total, element) => total/element )
 }
 
 
 
 
 
-module.exports = main;
+module.exports = {
+    math
+};
--- project_calculator/code/main.spec.js
+++ project_calculator/code/main.spec.js
@@ -1,7 +1,19 @@
-const main = require('./main')
+const calculator = require('./main')
 
-describe('main', () => {
-  test.skip('works with century years', () => {
-    expect(leapYears(700)).toBe(false);
+describe('math', () => {
+  test('add', () => {
+    expect(calculator.math("+",1, 2)).toBe(3);
+  });
+
+  test('sub', () => {
+    expect(calculator.math("-",6, 5)).toBe(1);
+  });
+
+  test('multiply', () => {
+    expect(calculator.math("*",3, 2)).toBe(6);
+  });
+
+  test('divide', () => {
+    expect(calculator.math("/",6, 3)).toBe(2);
   });
 });
